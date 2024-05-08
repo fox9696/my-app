6261d0660afd0e1e3f8719a380c929f8738d39e6
surkanghyun <qorrnfn2000@naver.com>
2024-05-08 12:36:58 +0900

    임시커밋

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthContextProvider } from './components/store/auth-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//App.js를 context의 Consumer로 사용하기위해
-//따로 분활해 놓은 AuthContextProvider의 children으로 전달.
-
-root.render(
-  <AuthContextProvider>
-    <App />
-  </AuthContextProvider>,
-);
+root.render(<App />);
